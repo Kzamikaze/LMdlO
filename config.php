d1e20e1c04c1edc726435ba9f406ac7227c11d17
Kzamikaze <a@a.a>
2017-03-27 11:49:28 +0200

    config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 # CMS Made Simple Configuration File
 # Documentation: /doc/CMSMS_config_reference.pdf
 #
-$config['dbms'] = 'mysqli';
-$config['db_hostname'] = '91.216.107.161';
-$config['db_username'] = 'lamai573266';
-$config['db_password'] = 'casama4321';
-$config['db_name'] = 'lamai573266';
+$config['dbms'] = 'mssql_odbc';
+$config['db_hostname'] = 'sqllmdlo.database.windows.net';
+$config['db_username'] = 'Kzamikaze';
+$config['db_password'] = 'julien2509';
+$config['db_name'] = 'lmdlo';
 $config['db_prefix'] = 'cms_';
 $config['timezone'] = 'Europe/Paris';
 ?>
