e1f10c2b5abff51f8f949d7266bce4e304eec4ab
Kzamikaze <a@a.a>
2017-03-27 12:14:11 +0200

    Vas te coucher

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 #
 $config['dbms'] = 'mssql';
 $config['db_hostname'] = 'sqllmdlo.database.windows.net';
-$config['db_username'] = 'Kzamikaze';
+$config['db_username'] = 'Kzamikaze2509';
 $config['db_password'] = 'Julien2509';
 $config['db_name'] = 'lmdlo';
 $config['db_prefix'] = 'cms_';
