cd0829848ebf5d9ce107ea11d64d0de810d055c8
Kzamikaze <a@a.a>
2017-03-27 11:55:49 +0200

    config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 $config['dbms'] = 'mssql_odbc';
 $config['db_hostname'] = 'sqllmdlo.database.windows.net';
 $config['db_username'] = 'Kzamikaze';
-$config['db_password'] = 'julien2509';
+$config['db_password'] = 'Julien2509';
 $config['db_name'] = 'lmdlo';
 $config['db_prefix'] = 'cms_';
 $config['timezone'] = 'Europe/Paris';
